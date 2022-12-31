33964dcb3454b7b342ef71cdf0348008e98f9ca3
Sergey Kozlov <104564146+kozlovs@users.noreply.github.com>
2022-12-31 18:47:50 +1000

    Реализовано отображение информации в StatsView

--- app/src/main/java/ru/netology/customviewapplication/MainActivity.kt
+++ app/src/main/java/ru/netology/customviewapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology.customviewapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.customviewapplication.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<StatsView>(R.id.statsView).data = listOf(
+            0.25F, 0.25F,
+        )
     }
 }
--- app/src/main/java/ru/netology/customviewapplication/ui/StatsView.kt
+++ app/src/main/java/ru/netology/customviewapplication/ui/StatsView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import ru.netology.customviewapplication.utils.AndroidUtils
 import kotlin.math.min
+import kotlin.random.Random
 
 class StatsView @JvmOverloads constructor(
     context: Context,
@@ -20,8 +22,15 @@ class StatsView @JvmOverloads constructor(
     defStyleAttr,
     defStyleRes
 ) {
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
     private var radius = 0F
     private var center = PointF()
+    private var oval = RectF()
     private val lineWidth = AndroidUtils.dp(context, 5)
     private val paint = Paint(
         Paint.ANTI_ALIAS_FLAG
@@ -32,12 +41,43 @@ class StatsView @JvmOverloads constructor(
         strokeCap = Paint.Cap.ROUND
     }
 
+    private val textPaint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        textSize = AndroidUtils.dp(context, 20).toFloat()
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+    }
+
     override fun onDraw(canvas: Canvas) {
-        canvas.drawCircle(center.x, center.y, radius, paint)
+        if (data.isEmpty()) {
+            return
+        } else {
+            var startAngle = -90F
+            data.forEach {
+                val angle = it * 360F
+                paint.color = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+                canvas.drawArc(oval, startAngle, angle, false, paint)
+                startAngle += angle
+            }
+        }
+
+        canvas.drawText(
+            "%.2f%%".format(data.sum() * 100),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint
+        )
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         radius = min(w, h) / 2F - AndroidUtils.dp(context, 5)
         center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius,
+            center.y - radius,
+            center.x + radius,
+            center.y + radius
+        )
     }
 }
