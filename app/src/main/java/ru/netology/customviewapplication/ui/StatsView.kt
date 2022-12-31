6b27cf970872e8f91800dc72c95b69c908f7b917
Sergey Kozlov <104564146+kozlovs@users.noreply.github.com>
2022-12-31 16:59:03 +1000

    Создан View в виде круга

---
+++ app/src/main/java/ru/netology/customviewapplication/ui/StatsView.kt
@@ -0,0 +1,43 @@
+package ru.netology.customviewapplication.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.View
+import ru.netology.customviewapplication.utils.AndroidUtils
+import kotlin.math.min
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(
+    context,
+    attributeSet,
+    defStyleAttr,
+    defStyleRes
+) {
+    private var radius = 0F
+    private var center = PointF()
+    private val lineWidth = AndroidUtils.dp(context, 5)
+    private val paint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = lineWidth.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawCircle(center.x, center.y, radius, paint)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - AndroidUtils.dp(context, 5)
+        center = PointF(w / 2F, h / 2F)
+    }
+}
