130308d1a2fdafe389b488915d9ab97b5ef8ffa9
Sergey Kozlov <104564146+kozlovs@users.noreply.github.com>
2023-01-03 14:51:23 +1000

    Создана анимация

--- app/src/main/java/ru/netology/customviewapplication/MainActivity.kt
+++ app/src/main/java/ru/netology/customviewapplication/MainActivity.kt
@@ -2,14 +2,38 @@ package ru.netology.customviewapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import ru.netology.customviewapplication.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(
             0.5F, 0.5F, 0.25F, 0.25F,
+        )
+        val textView = findViewById<TextView>(R.id.label)
+
+        view.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
+                setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {
+                        textView.text = "onAnimationStart"
+                    }
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        textView.text = "onAnimationEnd"
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+                        textView.text = "onAnimationEnd"
+                    }
+
+                })
+            }
         )
     }
 }
